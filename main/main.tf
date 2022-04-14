7fc08b10a77c5d2d3893314a0789ea25a15e9c54
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-04-14 11:52:17 -0500

    Modificacion bug

--- main/main.tf
+++ main/main.tf
@@ -36,11 +36,11 @@ resource "azurerm_kubernetes_cluster" "kallsony_aks" {
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config.0.client_certificate
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config_raw
 
   sensitive = true
 }

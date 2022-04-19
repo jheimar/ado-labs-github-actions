c3385f184ba56714b600a961764c785a19e3a4f0
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-04-18 19:00:00 -0500

    Deploy Aks

--- main/main.tf
+++ main/main.tf
@@ -14,34 +14,33 @@ resource "random_integer" "name_suffix" {
   max = 99999
 }
 
-#resource "azurerm_kubernetes_cluster" "kallsony_aks" {
-#  name                = "kallsony-aks1"
-#  location            = "eastus"
-#  resource_group_name = "kallsony_rg"
-#  dns_prefix          = "kallsonyks1"
-#  default_node_pool {
-#    name       = "default"
-#    node_count = 1
-#   vm_size    = "Standard_D2_v2"
-#  }
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#
-#  tags = {
-#    Environment = "Dev"
-#  }
-#}
+resource "azurerm_kubernetes_cluster" "kallsony_aks" {
+  name                = "kallsony-aks1"
+  location            = "eastus"
+  resource_group_name = "kallsony_rg"
+  dns_prefix          = "kallsonyks1"
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+   vm_size    = "Standard_D2_v2"
+  }
 
-#output "client_certificate" {
-#  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config.0.client_certificate
-#}
+  identity {
+    type = "SystemAssigned"
+  }
 
-#output "kube_config" {
-#  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config_raw
-#  sensitive = true
-#}
+  tags = {
+    Environment = "Dev"
+  }
+}
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config_raw
+  sensitive = true
+}
 
 ##################################################################################
 # APP SERVICE

991c073a353260fc2adfd0cdb23a15fe6e22fab0
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-04-21 19:50:22 -0500

    Destroy BD

--- main/main.tf
+++ main/main.tf
@@ -42,25 +42,25 @@ resource "random_integer" "name_suffix" {
 #}
 
 
-resource "azurerm_mssql_server" "msqlserver" {
-  name                         = "kallsony-sqlserver"
-  location                     = "eastus"
-  resource_group_name          = "kallsony_rg"
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-}
+#resource "azurerm_mssql_server" "msqlserver" {
+#  name                         = "kallsony-sqlserver"
+#  location                     = "eastus"
+#  resource_group_name          = "kallsony_rg"
+#  version                      = "12.0"
+#  administrator_login          = "4dm1n157r470r"
+#  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+#}
 
-resource "azurerm_mssql_database" "sqldb" {
-  name           = "acctest-db-d"
-  server_id      = azurerm_mssql_server.msqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "BC_Gen5_2"
-  zone_redundant = true
-}
+#resource "azurerm_mssql_database" "sqldb" {
+#  name           = "acctest-db-d"
+#  server_id      = azurerm_mssql_server.msqlserver.id
+#  collation      = "SQL_Latin1_General_CP1_CI_AS"
+#  license_type   = "LicenseIncluded"
+# max_size_gb    = 4
+ # read_scale     = true
+#  sku_name       = "BC_Gen5_2"
+#  zone_redundant = true
+#}
 
 #resource "azurerm_container_registry" "acr" {
 #  name                = "acrKallSony"

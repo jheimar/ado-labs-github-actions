1feefeec2b3e22b8bf03b22efbf04749ded1a206
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-01 18:35:42 -0500

    Deploy aks

--- main/main.tf
+++ main/main.tf
@@ -3,7 +3,7 @@
 ##################################################################################
 
 
-locals {
+/*locals {
   resource_group_name   = "${var.naming_prefix}-${random_integer.name_suffix.result}"
   app_service_plan_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
   app_service_name      = "${var.naming_prefix}-${random_integer.name_suffix.result}"
@@ -12,34 +12,33 @@ locals {
 resource "random_integer" "name_suffix" {
   min = 10000
   max = 99999
+}*/
+
+resource "azurerm_kubernetes_cluster" "kallsony_aks" {
+  name                = "kallsony-aks1"
+  location            = "eastus"
+  resource_group_name = "kallsony_rg"
+  dns_prefix          = "kallsonyks1"
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    Environment = "Dev"
+  }
+}
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config.0.client_certificate
 }
 
-#resource "azurerm_kubernetes_cluster" "kallsony_aks" {
-#  name                = "kallsony-aks1"
-#  location            = "eastus"
-#  resource_group_name = "kallsony_rg"
-##  dns_prefix          = "kallsonyks1"
-#  default_node_pool {
-#    name       = "default"
-#    node_count = 1
-#    vm_size    = "Standard_D2_v2"
-#  }
-#  identity {
-#    type = "SystemAssigned"
-#  }
-
-#  tags = {
-#    Environment = "Dev"
-#  }
-#}
-#output "client_certificate" {
-#  value = azurerm_kubernetes_cluster.kallsony_aks.kube_config.0.client_certificate
-#}
-
-#output "kube_config" {
-#  value     = azurerm_kubernetes_cluster.kallsony_aks.kube_config_raw
-#  sensitive = true
-#}
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.kallsony_aks.kube_config_raw
+  sensitive = true
+}
 
 
 #resource "azurerm_mssql_server" "msqlserver" {

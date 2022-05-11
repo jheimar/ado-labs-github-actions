023d922d8ebd847c0ef7233552053534d1927c92
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-10 20:58:04 -0500

    Destroy mysql

--- main/main.tf
+++ main/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }*/
 
-resource "azurerm_mysql_server" "mysql_server" {
+/*resource "azurerm_mysql_server" "mysql_server" {
   name                = "kallsony-db"
   location            = "eastus2"
   resource_group_name = "kallsony_rg"
@@ -82,7 +82,7 @@ resource "azurerm_mysql_firewall_rule" "firewall_rule_mysql" {
   server_name         = azurerm_mysql_server.mysql_server.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
-}
+}*/
 
 
 

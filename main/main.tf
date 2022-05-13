2f6939b88f62eed1d73fc354dc5eedb319c10422
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-13 14:51:29 -0500

    Destroy aks

--- main/main.tf
+++ main/main.tf
@@ -13,7 +13,7 @@ resource "random_integer" "name_suffix" {
   min = 10000
   max = 99999
 }*/
-
+/*
 resource "azurerm_kubernetes_cluster" "kallsony_aks" {
   name                = "kallsony-aks1"
   location            = "eastus"

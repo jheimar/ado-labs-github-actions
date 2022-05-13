ba7a4fc0c4feb3ec10161f16e8d657fdbd24e60c
LAPTOP-UFEFEDM5\jhprieto <jheimar@gmail.com>
2022-05-12 19:06:55 -0500

    Apply aks

--- main/main.tf
+++ main/main.tf
@@ -13,7 +13,7 @@ resource "random_integer" "name_suffix" {
   min = 10000
   max = 99999
 }*/
-/*
+
 resource "azurerm_kubernetes_cluster" "kallsony_aks" {
   name                = "kallsony-aks1"
   location            = "eastus"
@@ -47,7 +47,7 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = false
 }
-
+/*
 resource "azurerm_mysql_server" "mysql_server" {
   name                = "kallsony-db"
   location            = "eastus2"
